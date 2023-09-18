0f3afab6c1bbed84910098943274a3dbab70193d
Ratinho253 <jv147002@gmail.com>
2023-09-18 16:45:45 -0300

    arrumando o date

--- app/src/main/java/br/senai/sp/jandira/s_book/components/create_account/components/caixaData.kt
+++ app/src/main/java/br/senai/sp/jandira/s_book/components/create_account/components/caixaData.kt
@@ -2,11 +2,14 @@ package br.senai.sp.jandira.s_book.components.create_account.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusEvent
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
@@ -58,7 +62,7 @@ fun caixa() {
         onValueChange = { },
         modifier = Modifier
             .padding(8.dp)
-            .fillMaxWidth()
+            .width(300.dp)
             .onFocusEvent {
                 if (it.isFocused) {
                     showDatePickerDialog = true
@@ -68,7 +72,7 @@ fun caixa() {
         label = {
             Text("Date")
         },
-        readOnly = true
+        readOnly = true,
     )
 }
 
