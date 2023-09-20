4ebfcae52c96d3a90e2ab245217321600935e72c
Felipe_Graciano <felipinhograciano@gmail.com>
2023-09-20 19:49:55 -0300

    troquei a função

--- app/src/main/java/br/senai/sp/jandira/s_book/repository/UserCategoryRepository.kt
+++ app/src/main/java/br/senai/sp/jandira/s_book/repository/UserCategoryRepository.kt
@@ -1,6 +1,8 @@
 package br.senai.sp.jandira.s_book.repository
 
+import br.senai.sp.jandira.s_book.model.Genero
 import br.senai.sp.jandira.s_book.service.RetrofitHelper
+import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import org.json.JSONObject
 import retrofit2.Response
@@ -9,11 +11,21 @@ class UserCategoryRepository {
 
     private val apiService = RetrofitHelper.userCategoryService()
 
-    suspend fun usuarioCategoria(id_usuario: Int, generos_preferidos: List<JSONObject>): Response<JsonObject>{
+    suspend fun usuarioCategoria(id_usuario: Int, generos_preferidos: List<Genero>): Response<JsonObject> {
+
+        val generosJsonArray = JsonArray()
+
+        for (genero in generos_preferidos) {
+            val generoJsonObject = JsonObject().apply {
+                addProperty("id", genero.id)
+                addProperty("nome", genero.nome)
+            }
+            generosJsonArray.add(generoJsonObject)
+        }
 
         val requestBody = JsonObject().apply {
             addProperty("id_usuario", id_usuario)
-            addProperty("generos_preferidos", generos_preferidos.toString())
+            add("generos_preferidos", generosJsonArray)
         }
 
         return apiService.inserirCategoriasdoUsuario(requestBody)
