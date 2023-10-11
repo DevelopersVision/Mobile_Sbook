0f19b3f4edf13ef28bb0dbee75a4c04d79e2fea4
FELIPE GRACIANO BERTANHA DOS SANTOS <22282790@dsn127m1246731.cfp127aluno.local>
2023-10-11 16:39:48 -0300

    add felipe

--- app/src/main/java/br/senai/sp/jandira/s_book/components/filterGenero/screen/FilterGeneroScreen.kt
+++ app/src/main/java/br/senai/sp/jandira/s_book/components/filterGenero/screen/FilterGeneroScreen.kt
@@ -1,5 +1,6 @@
 package br.senai.sp.jandira.s_book.components.filterGenero.screen
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -9,8 +10,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,12 +25,42 @@ import androidx.navigation.NavController
 import br.senai.sp.jandira.s_book.components.filterGenero.components.CheckGenero
 import br.senai.sp.jandira.s_book.components.universal.HeaderFilter
 import br.senai.sp.jandira.s_book.components.universal.SearchFilter
+import br.senai.sp.jandira.s_book.model.AnunciosFavoritosBaseResponse
+import br.senai.sp.jandira.s_book.model.Genero
+import br.senai.sp.jandira.s_book.model.JsonFavoritados
+import br.senai.sp.jandira.s_book.repository.CategoryList
+import br.senai.sp.jandira.s_book.service.RetrofitHelper
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-@Preview(showSystemUi = true)
 @Composable
 fun FilterGeneroScreen(
     navController: NavController
 ){
+    var listGeneros by remember{
+        mutableStateOf(listOf<Genero>())
+    }
+
+    val call = RetrofitHelper.getCategoryService().getGeneros()
+
+
+    // Executar a chamada
+    call.enqueue(object : Callback<CategoryList> {
+        override fun onResponse(
+            call: Call<CategoryList>,
+            response: Response<CategoryList>
+        ) {
+            listGeneros = response.body()!!.dados
+
+        }
+
+
+        override fun onFailure(call: Call<CategoryList>, t: Throwable) {
+
+        }
+    })
+
     Surface(
         modifier = Modifier
             .fillMaxSize()
@@ -50,15 +86,9 @@ fun FilterGeneroScreen(
                 modifier = Modifier
                     .fillMaxSize()
             ) {
-                items(9) {
+                items(listGeneros) {
                     CheckGenero(
-                        text = "Terror"
-                    )
-                    CheckGenero(
-                        text = "Romance"
-                    )
-                    CheckGenero(
-                        text = "Ação"
+                        text = it.nome
                     )
                 }
             }
