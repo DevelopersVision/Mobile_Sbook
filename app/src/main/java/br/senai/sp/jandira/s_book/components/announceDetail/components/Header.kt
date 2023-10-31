8e5c2ecda868a7855ddce0ad81e03b84af06fc30
Ratinho253 <jv147002@gmail.com>
2023-10-31 14:16:25 -0300

    terminei o for do carrossel, está pegando sem erro nenhum

--- app/src/main/java/br/senai/sp/jandira/s_book/components/announceDetail/components/Header.kt
+++ app/src/main/java/br/senai/sp/jandira/s_book/components/announceDetail/components/Header.kt
@@ -2,43 +2,26 @@ package br.senai.sp.jandira.s_book.components.announceDetail.components
 
 import android.util.Log
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Circle
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import br.senai.sp.jandira.s_book.R
 import br.senai.sp.jandira.s_book.view_model.AnuncioViewModel
 import coil.compose.AsyncImage
+import androidx.compose.foundation.pager.rememberPagerState
 
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -46,56 +29,72 @@ import coil.compose.AsyncImage
 fun Header(viewMODEL: AnuncioViewModel) {
     val TAG = "Teste"
 
-    Column (
+    Column(
         modifier = Modifier
             .height(288.dp)
             .width(160.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.Top),
         horizontalAlignment = Alignment.CenterHorizontally,
-    ){
-        val pagerState = rememberPagerState(
-            pageCount = {
-                4
-            }
-        )
-        Card(
-            modifier = Modifier
-                .height(268.dp)
-                .width(160.dp)
-        ) {
-            Log.e(TAG, "Header: ${viewMODEL.foto}", )
-            val imagens = remember{
-                mutableStateListOf(
-                    viewMODEL.foto[0].foto,
-                    viewMODEL.foto[1].foto,
-                    viewMODEL.foto[2].foto,
-                    viewMODEL.foto[3].foto
-                )
-            }
-            Log.e(TAG, "Header: $imagens", )
-            HorizontalPager(
-                modifier = Modifier,
-                state = pagerState,
-                pageSpacing = 0.dp,
-                pageContent = {
-                    Row (
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier
-                            .fillMaxSize()
+    ) {
 
-                    ){
-                        AsyncImage(
-                            model = imagens[it],
-                            contentDescription = "",
-                            contentScale = ContentScale.Crop,
-                            modifier = Modifier
-                                .width(168.dp)
-                                .height(245.dp)
-                                .padding(top = 12.dp)
-                        )
-                    }
+        Column(
+            modifier = Modifier
+                .height(288.dp)
+                .width(160.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.Top),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            val imagens = remember { mutableStateListOf<String>() }
+            imagens.clear()
+
+            val fotos = viewMODEL.foto
+
+            for (foto in fotos) {
+                val imagemUrl = foto.foto
+
+                if (imagemUrl.isNotEmpty()) {
+                    imagens.add(imagemUrl)
+                }
+            }
+            val pagerState = rememberPagerState(
+                pageCount = {
+                    imagens.size
                 }
             )
+
+            Card(
+                modifier = Modifier
+                    .height(268.dp)
+                    .width(160.dp)
+            ) {
+                Log.e(TAG, "Header: $imagens")
+
+                HorizontalPager(
+                    modifier = Modifier,
+                    state = pagerState,
+                    pageSpacing = 0.dp,
+                    pageContent = { pageIndex ->
+                        val imagem = imagens.getOrNull(pageIndex)
+
+                        imagem?.let {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier.fillMaxSize()
+                            ) {
+                                AsyncImage(
+                                    model = it,
+                                    contentDescription = "",
+                                    contentScale = ContentScale.Crop,
+                                    modifier = Modifier
+                                        .width(168.dp)
+                                        .height(245.dp)
+                                        .padding(top = 12.dp)
+                                )
+                            }
+                        }
+                    }
+                )
+            }
         }
     }
 }
