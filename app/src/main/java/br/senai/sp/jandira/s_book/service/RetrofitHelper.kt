0fbf4c17c66de815fe0b0b63c71aa8ea12de41fe
FELIPE GRACIANO BERTANHA DOS SANTOS <22282790@dsn127m1246731.cfp127aluno.local>
2023-11-29 16:17:32 -0300

    A joao

--- app/src/main/java/br/senai/sp/jandira/s_book/service/RetrofitHelper.kt
+++ app/src/main/java/br/senai/sp/jandira/s_book/service/RetrofitHelper.kt
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit
 object RetrofitHelper {
 
     // private const val baseurl = "http://192.168.0.108:8080"
-    private const val baseurl = "http://10.107.144.5:8080"
+    private const val baseurl = "http://10.107.144.12:8080"
     //private const val baseurl = "http://26.254.150.48:8080"
 
     object HttpClientProvider {
