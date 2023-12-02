9eab3c2f9e3aee4d4e42dc7f73008d3764b5d46c
luyz-gusta <luizgustavo.sp2020@gmail.com>
2023-12-01 23:01:46 -0300

    Nova url

--- app/src/main/java/br/senai/sp/jandira/s_book/service/RetrofitHelper.kt
+++ app/src/main/java/br/senai/sp/jandira/s_book/service/RetrofitHelper.kt
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitHelper {
 
-     private const val baseurl = "http://192.168.0.108:8080"
+     private const val baseurl = "https://sbookapp.azurewebsites.net"
 //    private const val baseurl = "http://10.107.144.5:8080"
 //    private const val baseurl = "http://26.254.150.48:8080"
 
